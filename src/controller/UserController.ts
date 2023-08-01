ae6d77171936fda97e16017fd5d761cc3bb6de22
Regiane-Martins <regianegmartins@hotmail.com>
2023-08-01 16:30:32 -0300

    criando endpoints users e pasta business

---
+++ src/controller/UserController.ts
@@ -0,0 +1,89 @@
+import express, { Request, Response } from 'express'
+import { UserDatabase } from '../database/UserDatabase'
+import { UserCreate, UserDB, UserLogin } from '../types'
+import { USER_ROLES, User } from '../models/User'
+import { UserBusiness } from '../business/UserBusiness'
+
+export class UserController {
+    public async signup(req: Request, res: Response): Promise<void> {
+        try {
+
+            const input: UserCreate = {
+                id: req.body.id as string,
+                name: req.body.name as string,
+                email: req.body.email as string,
+                password: req.body.password as string,
+            }
+
+            const userBusiness = new UserBusiness()
+            const output = await userBusiness.signup(input)
+
+            res.status(201).send(output)
+
+        } catch (error) {
+            console.log(error)
+
+            if (req.statusCode === 200) {
+                res.status(500)
+            }
+
+            if (error instanceof Error) {
+                res.send(error.message)
+            } else {
+                res.send("Erro inesperado")
+            }
+        }
+    }
+
+    public async getAllUsers(req: Request, res: Response): Promise<void> {
+        try {
+
+            const userBusiness = new UserBusiness()
+            const output = await userBusiness.getAllUsers()
+
+            res.status(200).send(output)
+
+        } catch (error) {
+            console.log(error)
+
+            if (req.statusCode === 200) {
+                res.status(500)
+            }
+
+            if (error instanceof Error) {
+                res.send(error.message)
+            } else {
+                res.send("Erro inesperado")
+            }
+        }
+    }
+
+    public async login(req: Request, res: Response): Promise<void> {
+        try {
+            const { email, password } = req.body
+
+            const input: UserLogin = {
+                email: req.body.email,
+                password: req.body.password
+            }
+
+            const userBusiness = new UserBusiness()
+            const output = await userBusiness.login(input)
+
+            res.status(200).send(output)
+
+        } catch (error) {
+            console.log(error)
+
+            if (req.statusCode === 200) {
+                res.status(500)
+            }
+
+            if (error instanceof Error) {
+                res.send(error.message)
+            } else {
+                res.send("Erro inesperado")
+            }
+        }
+    }
+}
---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export interface UserDB {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    created_at: string
+}
+
+export interface UserCreate {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+}
+
+export interface UserLogin{
+    email: string,
+    password: string,
+}
+
+export interface PostDB {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number
+    dislikes: number,
+    created_at: string,
+    updated_at: string
+}
+
+
